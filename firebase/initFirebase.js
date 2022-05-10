194c531ce6d4c9474e84658d7e0fbeca4fc63989
juneroyquinimon <juneroyquinimon@ocomsoft.com>
2022-05-10 19:32:28 +0800

    add firebase

---
+++ firebase/initFirebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+import 'firebase/analytics'
+import 'firebase/performance'
+
+
+const clientCredentials = {
+    
+}
